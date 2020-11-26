0182a750f181003ed9ec9015b582f7a6a9d07167
ali <rima.aneta@gmail.com>
2020-11-26 23:19:25 +0100

    new initial

---
+++ Bowling.Test/BowlingApp/Round.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingApp
+{
+    class Round
+    {
+        public void Roll(int pins)
+        {
+
+        }
+        public int Score()
+        {
+            return 0;
+        }
+    }
+}
