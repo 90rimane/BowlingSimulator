a9ade3460211ce255b362eacd868b864b1b11c0e
ali <rima.aneta@gmail.com>
2020-11-27 18:32:42 +0100

    creating methods by TestMethod

--- BowlingGameTest/BowlingTest.cs
+++ BowlingGameTest/BowlingTest.cs
@@ -25,7 +25,19 @@ namespace BowlingGameTest
 
             Assert.AreEqual(20, game.Score);
         }
-        
+        [TestMethod]
+        public void TestOneSpare()
+        {
+            var game = SetupRound();
+
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(3);
+            RollPins(game, 17, 0);
+
+            Assert.AreEqual(16, game.Score);
+        }
+
         private void RollPins(Round game,int RollsNumber, int HittedPinsPerRoll)
         {
             for (int i = 0; i < RollsNumber; i++)
--- BowlingLibrary/Round.cs
+++ BowlingLibrary/Round.cs
@@ -6,15 +6,40 @@ namespace BowlingLibrary
 {
     public class Round
     {
-        public Round()
-        {
-            Score = 0;
-        }
+        private List<int> _rolls = new List<int>(21);
+       
 
         public void Roll(int pins)
         {
-            Score += pins;
+            _rolls.Add(pins) ;
         }
-        public int Score { get; set; }
+        public int Score 
+        {
+            get
+            {
+                int score = 0;
+                int frameIndex = 0;
+                for (int frame = 0; frame<10 ; frame++)
+                {
+                    if (IsSpare(frameIndex))
+                    {
+                        //spare
+                        score += 10 + _rolls[frameIndex + 2];
+                        frameIndex += 2;
+                    }
+                    else
+                    {
+                        score += _rolls[frameIndex] + _rolls[frameIndex + 1];
+                        frameIndex += 2;
+                    }
+                }
+                return score;
+            }
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return _rolls[frameIndex] + _rolls[frameIndex + 1] == 10;
+        }
     }
 }
