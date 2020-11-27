eab21b32195a965a3136766918b30f42ce5e8d89
ali <rima.aneta@gmail.com>
2020-11-27 19:04:03 +0100

    completing methods

--- BowlingGameTest/BowlingTest.cs
+++ BowlingGameTest/BowlingTest.cs
@@ -30,12 +30,35 @@ namespace BowlingGameTest
         {
             var game = SetupRound();
 
-            game.Roll(5);
-            game.Roll(5);
+            RollSpare(game);
             game.Roll(3);
+
             RollPins(game, 17, 0);
 
             Assert.AreEqual(16, game.Score);
+        }
+        [TestMethod]
+        public void TestOneStrike()
+        {
+            var game = SetupRound();
+
+            RollStrike(game);
+            game.Roll(3);
+            game.Roll(4);
+            RollPins(game, 16, 0);
+
+            Assert.AreEqual(24, game.Score);
+        }
+
+        private void RollStrike(Round game)
+        {
+            game.Roll(10);
+        }
+
+        private void RollSpare(Round game)
+        {
+            game.Roll(5);
+            game.Roll(5);
         }
 
         private void RollPins(Round game,int RollsNumber, int HittedPinsPerRoll)
--- BowlingLibrary/Round.cs
+++ BowlingLibrary/Round.cs
@@ -7,11 +7,18 @@ namespace BowlingLibrary
     public class Round
     {
         private List<int> _rolls = new List<int>(21);
-       
+        private int _currentRoll = 0;
+        public Round()
+        {
+            for (int i = 0; i < 22; i++)
+            {
+                _rolls.Add(0);
+            }
+        }
 
         public void Roll(int pins)
         {
-            _rolls.Add(pins) ;
+            _rolls[_currentRoll++] = pins;
         }
         public int Score 
         {
@@ -21,9 +28,13 @@ namespace BowlingLibrary
                 int frameIndex = 0;
                 for (int frame = 0; frame<10 ; frame++)
                 {
-                    if (IsSpare(frameIndex))
+                    if (IsStrike(frameIndex))
                     {
-                        //spare
+                        score += 10 + _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+                        frameIndex++;
+                    }
+                    else if (IsSpare(frameIndex))
+                    {
                         score += 10 + _rolls[frameIndex + 2];
                         frameIndex += 2;
                     }
@@ -35,6 +46,12 @@ namespace BowlingLibrary
                 }
                 return score;
             }
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return _rolls[frameIndex] == 10;
+                
         }
 
         private bool IsSpare(int frameIndex)
