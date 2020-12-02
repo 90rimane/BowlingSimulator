708c802dd7ce5b8df54f759b2777125ec63524de
ali <rima.aneta@gmail.com>
2020-12-02 10:56:19 +0100

    new classLibrary created for declarate name and total scores

---
+++ BowlingLibrary/People.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace BowlingLibrary
+{
+    public class People
+    {
+        public People(string name, int totalScore)
+        {
+            Name = name;
+            TotalScore = totalScore;
+        }
+        public string Name { get; set; }
+        public int TotalScore { get; set; }
+    }
+}
